449cd09687d59061af94b085a0d7a9f78ff44d9b
Ryan Vickers <Ryan.vickers98@hotmail.com>
2022-05-05 20:38:08 -0400

    -Added Messaging Form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import React, {useEffect, useState} from "react";
-import {db, auth} from "../firebase";
-import {collection, query, where, onSnapshot} from "firebase/firestore";
+import {db, auth, storage} from "../firebase";
+import {collection, query, where, onSnapshot, addDoc, Timestamp} from "firebase/firestore";
 import User from "../components/User"
+import MessageForm from "../components/MessageForm";
+import {ref,getDownloadURL,uploadBytes} from "firebase/storage";
 
 const Home = () => {
-    const [users, setUsers]= useState([]);
+    const [users, setUsers] = useState([]);
+    const [chat, setChat] = useState("");
+    const [text, setText] = useState('');
+    const [img, setImg] = useState("");
+
+    const user1 = auth.currentUser.uid
 
     useEffect(()=>{
         const usersRef = collection(db,'users')
         //database query
-        const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+        const q = query(usersRef, where("uid", "not-in", [user1]));
         const unsub = onSnapshot(q, querySnapshot => {
             let users = [];
             querySnapshot.forEach(doc=>{
@@ -18,16 +25,57 @@ const Home = () => {
             setUsers(users);
         });
         return () => unsub();
-    },[])
-    console.log(users);
+    },[]);
+
+    const selectUser = (user) => {
+        setChat(user);
+        console.log(user);
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const user2 = chat.uid
+        const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+
+        let url;
+        if(img){
+            const imgRef = ref(storage, `images/${new Date().getTime()} - ${img.name}`);
+            const snap = await uploadBytes(imgRef, img);
+            const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath));
+            url = dlUrl;
+        }
+
+        await addDoc(collection(db, 'messages', id, 'chat'), {
+            text,
+            from: user1,
+            to: user2,
+            createdAt: Timestamp.fromDate(new Date()),
+            media: url || "",
+        })
+        setText("");
+    }
+
     return(
         <div className="home_container">
             <div className="users_container">
                 {users.map((user) => (
-                     <User key={user.uid} user={user}/>
+                     <User key={user.uid} user={user} selectUser={selectUser}/>
                      ))}
             </div>
+            <div className="messages_container">
+                {chat ? (
+                    <>
+                <div className="messages_user">
+                    <h3>{chat.name}</h3>
+                </div>
+                <MessageForm handleSubmit={handleSubmit} text={text} setText={setText} setImg={setImg}/>
+                </>
+                ):(
+             <h3 className="no_convo">Select a User to Start a Conversation</h3>
+                )}
         </div>
+    </div>
     )
  }
    
