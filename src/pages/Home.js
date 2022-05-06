4679d73eca05d671d982530978f51cc02e80deb6
Ryan Vickers <Ryan.vickers98@hotmail.com>
2022-05-05 21:50:17 -0400

    -Added fuction to send Messages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {db, auth, storage} from "../firebase";
-import {collection, query, where, onSnapshot, addDoc, Timestamp} from "firebase/firestore";
+import {collection, query, where, orderBy, onSnapshot, addDoc, Timestamp} from "firebase/firestore";
 import User from "../components/User"
 import MessageForm from "../components/MessageForm";
+import Message from "../components/Message";
 import {ref,getDownloadURL,uploadBytes} from "firebase/storage";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
     const [chat, setChat] = useState("");
     const [text, setText] = useState('');
     const [img, setImg] = useState("");
+    const [msgs, setMsgs] = useState("");
 
     const user1 = auth.currentUser.uid
 
@@ -29,14 +31,28 @@ const Home = () => {
 
     const selectUser = (user) => {
         setChat(user);
-        console.log(user);
-    }
+
+        const user2 = user.uid
+        const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+
+        const msgsRef = collection(db, "messages", id, "chat");
+        const q = query(msgsRef, orderBy("createdAt", "asc"));
+
+        onSnapshot(q, (querySnapshot) => {
+            let msgs = [];
+            querySnapshot.forEach((doc) => {
+              msgs.push(doc.data());
+            });
+            setMsgs(msgs);
+        });
+    };
+    console.log(msgs)
 
     const handleSubmit = async e => {
         e.preventDefault();
 
         const user2 = chat.uid
-        const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+        const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
 
         let url;
         if(img){
@@ -46,37 +62,55 @@ const Home = () => {
             url = dlUrl;
         }
 
-        await addDoc(collection(db, 'messages', id, 'chat'), {
+        await addDoc(collection(db, "messages", id, "chat"), {
             text,
             from: user1,
             to: user2,
             createdAt: Timestamp.fromDate(new Date()),
             media: url || "",
-        })
+          });
         setText("");
     }
 
     return(
         <div className="home_container">
-            <div className="users_container">
-                {users.map((user) => (
-                     <User key={user.uid} user={user} selectUser={selectUser}/>
-                     ))}
-            </div>
-            <div className="messages_container">
-                {chat ? (
-                    <>
-                <div className="messages_user">
-                    <h3>{chat.name}</h3>
-                </div>
-                <MessageForm handleSubmit={handleSubmit} text={text} setText={setText} setImg={setImg}/>
-                </>
-                ):(
-             <h3 className="no_convo">Select a User to Start a Conversation</h3>
-                )}
+        <div className="users_container">
+          {users.map((user) => (
+            <User
+              key={user.uid}
+              user={user}
+              selectUser={selectUser}
+              user1={user1}
+              chat={chat}
+            />
+          ))}
         </div>
-    </div>
-    )
- }
+        <div className="messages_container">
+          {chat ? (
+            <>
+              <div className="messages_user">
+                <h3>{chat.name}</h3>
+              </div>
+              <div className="messages">
+              {msgs.length
+                ? msgs.map((msg, i) => (
+                    <Message key={i} msg={msg} user1={user1} />
+                  ))
+                : null}
+              </div>
+              <MessageForm
+                handleSubmit={handleSubmit}
+                text={text}
+                setText={setText}
+                setImg={setImg}
+              />
+            </>
+          ) : (
+            <h3 className="no_conv">Select a user to start conversation</h3>
+          )}
+        </div>
+      </div>
+    );
+  };
    
 export default Home;
